2438b39aae666a5197e882194203c804aa54149d
rkouidri <reuben-kouidri@hotmail.com>
2022-11-24 15:27:04 +0000

    fixed transform bug and annotated funcs

--- wavelets.py
+++ wavelets.py
@@ -1,14 +1,26 @@
 import pywt
 from skimage.transform import resize
+import numpy as np
 
 
-def mexh(signal, widths):
+def mexh(signal: np.ndarray, widths: np.ndarray) -> np.ndarray:
+    """
+    :param signal: 1D ndarray of the timeseries signal to be processed
+    :param widths: 1D ndarray of the widths that the CWT will be computed for
+    :return img: 2D ndarray representing the image of the transformed signal
+    """
     img, _ = pywt.cwt(signal, widths, "mexh")
-    img.resize((128, 128))
+    img = resize(img, (128, 128))
     return img
 
 
-def cmor(signal, widths):
+def cmor(signal: np.ndarray, widths: np.ndarray) -> np.ndarray:
+    """
+    uses the complex-Morlet CWT
+    :param signal: 1D ndarray of the timeseries signal to be processed
+    :param widths: 1D ndarray of the widths that the CWT will be computed for
+    :return img: 2D ndarray representing the image of the transformed signal
+    """
     img, _ = pywt.cwt(signal, widths, 'cmor1.5-1')
     img = abs(img)
     img = resize(img, (128, 128))
