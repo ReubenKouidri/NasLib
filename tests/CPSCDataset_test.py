e1cd105ab32efd75f94894e07312c8579d05cfe2
rkouidri <reuben-kouidri@hotmail.com>
2022-11-28 19:13:18 +0000

    updating test

--- tests/CPSCDataset_test.py
+++ tests/CPSCDataset_test.py
@@ -1,12 +1,63 @@
+import torch
+import torch.nn as nn
+from dnasty.components import ConvBlock2D, DenseBlock, Flatten, Tensor, CBAM
 from my_datasets.CPSCDataset import CPSCDataset, CPSCDataset2D
 import matplotlib.pyplot as plt
 
-DATA_PATH = "../my_datasets/Train300"
-REF_PATH = "../my_datasets/reference300.csv"
+DATA_PATH = "../my_datasets/cpsc_data/Train300"
+REF_PATH = "../my_datasets/cpsc_data/reference300.csv"
+
+
+class M2(nn.Module):
+    def __init__(self):
+        super(M2, self).__init__()
+        self.cbam = True
+        self.MP1 = nn.MaxPool2d(kernel_size=2)
+        self.MP2 = nn.MaxPool2d(kernel_size=2)
+        self.CB1 = ConvBlock2D(in_channels=1, out_channels=32, kernel_size=10, bn=True, activation='ReLU')
+        self.CB2 = ConvBlock2D(in_channels=32, out_channels=32, kernel_size=10, bn=True, activation='ReLU')
+        self.CB3 = ConvBlock2D(in_channels=32, out_channels=32, kernel_size=8, bn=True, activation='ReLU')
+        self.CB4 = ConvBlock2D(in_channels=32, out_channels=32, kernel_size=4, bn=True, activation='ReLU')
+        self.CBAM1 = CBAM(in_channels=32, se_ratio=4, kernel_size=4, spatial=True, channel=True)
+        self.CBAM2 = CBAM(in_channels=32, se_ratio=4, kernel_size=4, spatial=True, channel=True)
+        self.flatten = Flatten()
+        self.DB1 = DenseBlock(in_features=15488, out_features=100, activation='ReLU', dropout=True)
+        self.DB2 = DenseBlock(in_features=100, out_features=9, activation='ReLU', dropout=False)
+        self.softmax = nn.Softmax(dim=1)
+
+    def build_model(self) -> nn.Module:
+        model = nn.Sequential()
+        model.add_module("CB1", self.CB1)
+        model.add_module("CB2", self.CB2)
+        model.add_module("CBAM1", self.CBAM1) if self.cbam else ...
+        model.add_module("MP1", self.MP1)
+        model.add_module("CB3", self.CB3)
+        model.add_module("CB4", self.CB4)
+        model.add_module("CBAM2", self.CBAM2) if self.cbam else ...
+        model.add_module("MP2", self.MP2)
+        model.add_module("Flatten", self.flatten)
+        model.add_module("DB1", self.DB1)
+        model.add_module("DB2", self.DB2)
+        model.add_module("softmax", self.softmax)
+        return model
+
+    def forward(self, t: Tensor) -> Tensor:
+        return self.build_model()(t)
+
+
+from torch.utils.data import DataLoader
 
 d2 = CPSCDataset2D(data_path=DATA_PATH, reference_path=REF_PATH)
+img, tgt = d2[0]
+m = M2()
+pred = m(img)
+print(pred)
 
-for i in range(5):
+"""for i in range(5):
     img, tgt = d2[i]
     plt.matshow(img, cmap='Greys')
     plt.show()
+#tensor = torch.randn(10, 1, 128, 128)
+#print(tensor)
+#print(m(tensor))
+"""
