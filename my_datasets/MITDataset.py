f3c1184fd0897fc7aa36965ee292dc0c621b2351
rkouidri <reuben-kouidri@hotmail.com>
2022-12-02 17:36:06 +0000

    MIT dataset added

--- my_datasets/MITDataset.py
+++ my_datasets/MITDataset.py
@@ -1,84 +1,36 @@
-import wfdb
 from torch.utils.data import Dataset
-import matplotlib.pyplot as plt
-import numpy as np
+import pandas as pd
 import os
 
 
-"""
-Start:
-- get ECG signal
-- get beat locations and annotations
-
-Create labelled dataset:
-- save each raw ECG record and its label info in a separate CSV file
-- for each item:
-    - get list of peaks
-    - split up into 3-beat segments (between beats)
-    - do not include first or last beats
-    - 
+BASE_PATH = "/Users/juliettekouidri/Documents/Reuben/Projects/Python/NasLib/my_datasets/mitbih_database"
+ANNOT_ENDING = "annotations.txt"
+data_files = [file for file in os.listdir(BASE_PATH) if file.endswith(".csv")]
+data_files.sort()
+__names__ = [name for name, _ in (os.path.splitext(file) for file in data_files)]  # [100, 101, ...]
+__annots__ = [name + ANNOT_ENDING for name in __names__]  # [100annotation.txt, ...]
 
 
-"""
-
-BASE_DIR = "/Users/juliettekouidri/Documents/Reuben/Projects/Python/NasLib/my_datasets/mit_data"
-RECORDS_FN = "records.txt"
-NAMES_PATH = os.path.join(BASE_DIR, RECORDS_FN)
-FREQUENCY = 360
-
-
-def get_names(records_path: str) -> list:
-    with open(NAMES_PATH) as f:
-        records = [record.strip() for record in f.readlines()]
-
-    return records
-
-
-def get_signal(signal_path: str):
-    record = wfdb.rdrecord(signal_path, channels=[0])
-    d_signal = record.adc()  # analogue to digital conversion
-    d_signal = (d_signal - d_signal.min()) / (d_signal.max() - d_signal.min())  # normalise
-    return d_signal
-
-
-def extract_labels(filename):
+def segment(name):
     """
-    - extract the labels and their locations (peaks) for each beat and
-    - puts them into a lists
-    - rdann reads in an annotation file and returns an Annotation object which has lists
-    of beat locations and their associated labels
+    cut ECG into 3-beat segments, centred on beat of interest.
+    save the output along with its label as csv.
+
+    -use annot file to get locs of peaks
+    -ignore first & last peaks
+    -find locs of midpoints between (3n)th and (3n+1)th
+    -cut the data file in these locations
+    -any label other than 'N'ormal takes priority
+    -multilabels allowed: refine implementation at a later stage
     """
-    ann = wfdb.rdann(filename, 'atr', return_label_elements=['symbol'])
-    # These two lines return the symbol and the locations
-    label = ann.symbol
-    loc = ann.sample
-    return zip(label, loc)
-
-names: list[int] = get_names(NAMES_PATH)
-
-for i, name in enumerate(names):
-    path_abs = os.path.join(BASE_DIR, str(name))
-    signal = get_signal(path_abs)
-    if i == 0 or i == 1:
-        labels = tuple(extract_labels(path_abs))
-        print(len(labels))
-
-        """print(signal)
-        x = np.linspace(0, FREQUENCY * 4, FREQUENCY * 4)
-        signal = signal[: FREQUENCY * 4]
-        plt.plot(x, signal)
-        plt.show()"""
-
-
-
-
-
+    ...
 
 
 class MITBIHDataset(Dataset):
     """
     OG arrhythmia dataset from MIT-BIH
     """
+
     def __getitem__(self, item):
         ...
 
