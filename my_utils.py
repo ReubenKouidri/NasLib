3aa72d47d7f1049ed1990d4500c2c94a76332f07
rkouidri <reuben-kouidri@hotmail.com>
2023-01-11 20:24:29 +0000

    added kfold decorator

--- my_utils.py
+++ my_utils.py
@@ -1,5 +1,8 @@
 import numpy as np
+import functools
+from typing import Callable, Optional, Any
 import torch
+from torch.utils.data import Subset
 
 
 class ModelSave:
@@ -45,5 +48,64 @@ class ModelSave:
                 f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
         torch.save(model.state_dict(), self.path)
         self.val_loss_min = val_loss
+
+
+def split(func, k, ) -> list[tuple[Subset, Subset, Subset]]:
+    splits = []
+    dataset = func(*args, **kwargs)
+    chunk_size = len(dataset) // k
+
+    for i in range(k):
+        start_idx = i * chunk_size
+        end_idx = (i + 1) * chunk_size
+        dataset_length = len(dataset)
+
+        if end_idx != k - 1:
+            testset = Subset(dataset, indices=range(start_idx, end_idx))
+            valset = Subset(dataset, indices=range(end_idx, (end_idx + chunk_size)))
+            trainset = Subset(dataset, indices=list(list(range((end_idx + chunk_size), dataset_length)) +
+                                                    list(range(0, start_idx))))
+        else:
+            end_idx = 0
+            testset = Subset(dataset, indices=range(start_idx, len(dataset)))
+            valset = Subset(dataset, indices=range(end_idx, (end_idx + chunk_size)))
+            trainset = Subset(dataset, indices=list(range((end_idx + chunk_size), dataset_length - chunk_size)))
+
+        splits.append((trainset, valset, testset))
+
+    return splits
+
+
+
+def kfold_split(k: int = 10):
+    def decorate(func: Callable[..., Any]) -> Callable[..., Any]:
+        if isinstance(k, int):
+            if k < 1:
+                raise ValueError("k must be larger than 1 in k-fold split")
+
+        def split(*args, **kwargs) -> list[tuple[Subset, Subset, Subset]]:
+            splits = []
+            dataset = func(*args, **kwargs)
+            chunk_size = len(dataset) // k
+            for i in range(k):
+                start_idx = i * chunk_size
+                end_idx = (i + 1) * chunk_size
+                dataset_length = len(dataset)
+                if end_idx != k - 1:
+                    testset = Subset(dataset, indices=range(start_idx, end_idx))
+                    valset = Subset(dataset, indices=range(end_idx, (end_idx + chunk_size)))
+                    trainset = Subset(dataset, indices=list(list(range((end_idx + chunk_size), dataset_length)) +
+                                                            list(range(0, start_idx))))
+                else:
+                    end_idx = 0
+                    testset = Subset(dataset, indices=range(start_idx, len(dataset)))
+                    valset = Subset(dataset, indices=range(end_idx, (end_idx + chunk_size)))
+                    trainset = Subset(dataset, indices=list(range((end_idx + chunk_size), dataset_length - chunk_size)))
+
+                splits.append((trainset, valset, testset))
+            return splits
+        return split
+    return decorate
+
 
 
