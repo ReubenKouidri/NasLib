d20dcca3bdc1cf337641df6abbc03626493ea789
rkouidri <reuben-kouidri@hotmail.com>
2023-01-12 16:17:49 +0000

    stub for overloading

--- my_utils.pyi
+++ my_utils.pyi
@@ -2,6 +2,5 @@ from typing import overload, Callable, Any, Tuple
 
 @overload
 def kfold_split(k: int) -> Callable[..., Any]: ...
-
 @overload
 def kfold_split(k: int, r: Tuple[float, float, float]) -> Callable[..., Any]: ...
