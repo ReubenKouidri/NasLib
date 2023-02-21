2342b39020ad9bfad1e78d09c2a729c78078f160
rkouidri <reuben-kouidri@hotmail.com>
2023-02-21 18:10:49 +0000

    added methods and training, tensorboard. Needs test

--- trainer.py
+++ trainer.py
@@ -1,94 +1,97 @@
 import torch
-import torch.nn as nn
-import torch.optim as optim
+from torch.optim import SGD
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from my_utils.ksplit import load_2d_dataset, split_dataset
+from my_utils.my_utils import get_num_correct
 from my_utils.config import Config
+from models.model import Model
+import torch.nn as nn
 
 
 class Trainer:
-    def __init__(self, model: torch.nn.Module, config: Config, checkpoint_dir: str) -> None:
-        self.model = model
+    def __init__(self,
+                 data_path: str,
+                 reference_path: str,
+                 config: Config,
+                 checkpoint_dir: str,
+                 ksplit: tuple[int, tuple] | None = None
+                 ) -> None:
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.config = config
         self.checkpoint_dir = checkpoint_dir
+        self.data = self._load_data(data_path, reference_path, ksplit)
+        self.ksplit = ksplit
+        self.writer = SummaryWriter(log_dir='logs')
 
-        self.dataloaders = dataloaders
-        self.loss_fn = loss_fn
-        self.optimizer = optimizer
-        self.metric_fn = metric_fn
-        self.device = device
-        self.training_loss = []
-        self.validation_loss = []
-        self.training_metrics = []
-        self.validation_metrics = []
+    @staticmethod
+    def _load_data(dp, rp, ksplit):
+        dataset = load_2d_dataset(dp, rp)
+        if ksplit:
+            datasets = split_dataset(dataset, ksplit[0], ksplit[1])
+            return datasets
+        return dataset
 
-    def _load_data(self, split=True):
-        """
-        load dataset
-        option for kfold split
-        return tuple of datasets
-        """
+    def _train(self, model, trainloader, optimizer, criterion) -> tuple[float, float]:
+        model.train()
+        total_loss = 0.0
+        correct = 0
+        for imgs, tgts in trainloader:
+            optimizer.zero_grad()
+            imgs = imgs.to(self.device, non_blocking=True)
+            tgts = tgts.to(self.device, non_blocking=True)
+            preds = model(imgs)
+            loss = criterion(preds, tgts)
+            loss.backward()
+            correct += get_num_correct(preds, tgts)
+            total_loss += loss.item()
+            optimizer.step()
+
+        train_loss = total_loss / (len(trainloader.dataset) / trainloader.batch_size)
+        train_acc = correct / (len(trainloader.dataset))
+
+        return train_loss, train_acc
+
+    @torch.inference_mode()
+    def _eval(self, model, valloader, criterion) -> tuple[float, float]:
+        model.eval()
+        total_loss = 0.0
+        correct = 0
+        for imgs, tgts in valloader:
+            imgs = imgs.to(self.device, non_blocking=True)
+            tgts = tgts.to(self.device, non_blocking=True)
+            preds = model(imgs)
+            correct += get_num_correct(preds, tgts)
+            loss = criterion(preds, tgts)
+            total_loss += loss.item()
+
+        val_acc = correct / len(valloader.dataset)
+        val_loss = total_loss / (len(valloader.dataset) / valloader.batch_size)
+        return val_loss, val_acc
+
+    @torch.inference_mode()
+    def test(self, model):
         ...
 
+    def save_model(self): ...
 
+    def run(self):
+        for fold, triplet in enumerate(self.data):
+            model = Model()
+            assert len(triplet) == 3
+            assert isinstance(triplet, tuple)
 
-    def train(self, num_epochs):
-        for epoch in range(num_epochs):
-            epoch_training_loss = 0
-            epoch_training_metrics = 0
-            self.model.train()
-            for inputs, labels in self.dataloaders['train']:
-                inputs = inputs.to(self.device)
-                labels = labels.to(self.device)
-                self.optimizer.zero_grad()
-                outputs = self.model(inputs)
-                loss = self.loss_fn(outputs, labels)
-                loss.backward()
-                self.optimizer.step()
-                epoch_training_loss += loss.item()
-                epoch_training_metrics += self.metric_fn(outputs, labels)
+            train_loader = DataLoader(dataset=triplet[0], batch_size=self.config.train.batch_size, shuffle=True)
+            eval_loader = DataLoader(dataset=triplet[1], batch_size=self.config.eval.batch_size, shuffle=True)
+            test_loader = DataLoader(dataset=triplet[2], batch_size=len(triplet[2]), shuffle=False)
+            optimizer = SGD(model.parameters(), self.config.train.lr, momentum=self.config.train.momentum,
+                            nesterov=self.config.train.nesterov)
+            criterion = nn.CrossEntropyLoss()
 
-            self.training_loss.append(epoch_training_loss / len(self.dataloaders['train']))
-            self.training_metrics.append(epoch_training_metrics / len(self.dataloaders['train']))
-
-            epoch_validation_loss = 0
-            epoch_validation_metrics = 0
-            self.model.eval()
-            with torch.no_grad():
-                for inputs, labels in self.dataloaders['val']:
-                    inputs = inputs.to(self.device)
-                    labels = labels.to(self.device)
-                    outputs = self.model(inputs)
-                    loss = self.loss_fn(outputs, labels)
-                    epoch_validation_loss += loss.item()
-                    epoch_validation_metrics += self.metric_fn(outputs, labels)
-
-            self.validation_loss.append(epoch_validation_loss / len(self.dataloaders['val']))
-            self.validation_metrics.append(epoch_validation_metrics / len(self.dataloaders['val']))
-
-            if (epoch+1) % 10 == 0:
-                torch.save({
-                    'epoch': epoch + 1,
-                    'model_state_dict': self.model.state_dict(),
-                    'optimizer_state_dict': self.optimizer.state_dict(),
-                    'training_loss': self.training_loss,
-                    'validation_loss': self.validation_loss,
-                    'training_metrics': self.training_metrics,
-                    'validation_metrics': self.validation_metrics,
-                }, self.checkpoint_dir + '/checkpoint_' + str(epoch + 1) + '.pth')
-
-    def test(self):
-        self.model.eval()
-        with torch.no_grad():
-            test_loss = 0
-            test_metrics = 0
-            for inputs, labels in self.dataloaders['test']:
-                inputs = inputs.to(self.device)
-                labels = labels.to(self.device)
-                outputs = self.model(inputs)
-                loss = self.loss_fn(outputs, labels)
-                test_loss += loss.item()
-                test_metrics += self.metric_fn(outputs, labels)
-
-        test_loss = test_loss / len(self.dataloaders['test'])
-        test_metrics = test_metrics / len(self.dataloaders['test'])
-        print('Test Loss: {:.4f}'.format(test_loss))
-        print('Test Metrics: {:.4f}'.format(test_metrics))
+            for epoch in range(self.config.train.num_epochs):
+                tr_loss, tr_acc = self._train(model, train_loader, optimizer, criterion)
+                ev_loss, ev_acc = self._eval(model, eval_loader, criterion)
+                self.writer.add_scalar(f'trainingLoss/fold{fold}', tr_loss, epoch)
+                self.writer.add_scalar(f'trainingAcc/fold{fold}', tr_acc, epoch)
+                self.writer.add_scalar(f'validationLoss/fold{fold}', ev_loss, epoch)
+                self.writer.add_scalar(f'validationAcc/fold{fold}', tr_acc, epoch)
