b42a1b67e8dbaec8753751618ba49f15b0c6f661
rkouidri <reuben-kouidri@hotmail.com>
2023-02-27 20:25:12 +0000

    forgot to set dropout to False on final layer.. oops

--- models/modelNew.py
+++ models/modelNew.py
@@ -1,9 +1,21 @@
-import dnasty.components as components
+from dnasty.components import ConvBlock2D, MaxPool2D, CBAM, DenseBlock, Flatten
 import torch.nn as nn
+from collections import OrderedDict
 
 
 class Model(nn.Sequential):
     def __init__(self):
-        super(Model, self).__init__()
-
-    def build(self) -> None: ...
+        self.dict = OrderedDict([
+            ("ConvBlock1", ConvBlock2D(1, 32, 10, bn=True, activation="ReLU")),
+            ("ConvBlock2", ConvBlock2D(32, 32, 10, bn=True, activation="ReLU")),
+            ("MP1", MaxPool2D(2, 2)),
+            ("CBAM1", CBAM(32, se_ratio=4, kernel_size=4)),
+            ("ConvBlock3", ConvBlock2D(32, 32, 8, bn=True, activation="ReLU")),
+            ("ConvBlock4", ConvBlock2D(32, 32, 4, bn=True, activation="ReLU")),
+            ("MP2", MaxPool2D(2, 2)),
+            ("Flatten", Flatten()),
+            ("DenseBlock1", DenseBlock(15488, 100, activation="ReLU")),
+            ("DenseBlock2", DenseBlock(100, 9, activation="ReLU", dropout=False)),
+            ("Softmax", nn.Softmax(dim=1))
+        ])
+        super(Model, self).__init__(self.dict)
