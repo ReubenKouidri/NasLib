814fcce11fceac3f6e4ef20ee639629f115bc7f0
rkouidri <reuben-kouidri@hotmail.com>
2023-02-28 19:36:30 +0000

    test case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from my_utils.config import Config
 from trainer import Trainer
+#from models.modelNew import Model
 from models.model import Model
 import os
 import json
@@ -15,11 +16,10 @@ def load_json(json_file: str) -> dict:
 
 
 config = Config(load_json(config_path))
-num_splits = 2
-ratio_splits = (0.8, 0.2, 0.0)
+num_splits = 1
+ratio_splits = (0.1, 0.1, 0.8)
 
-trainer = Trainer(config=config, ksplit=(num_splits, ratio_splits))
-
+trainer = Trainer(config=config, split_ratio=(num_splits, ratio_splits))
 model = Model()
+trainer(model, epochs=1, output=True)
 
-trainer(model)
