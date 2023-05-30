128379c6a96f5fe6e7ae543d459f45fc8373bdc1
rkouidri <reuben-kouidri@hotmail.com>
2023-05-30 13:57:57 +0100

    added test_getattr

--- tests/test_DenseGene.py
+++ tests/test_DenseGene.py
@@ -1,35 +1,40 @@
-from dnasty.genetics import DenseGene
+from dnasty.genetics import LinearGene
 import unittest
 
 
-def test_mutation(gene):
-    for key in gene.exons.keys():
-        gene.exons[key] += 1
-
-
-class TestDenseGene(unittest.TestCase):
+class TestLinearGene(unittest.TestCase):
     def setUp(self):
-        self.exons = {"in_features": 1, "out_features": 2}
-        self.loc1 = 1
-        self.loc2= 2
-        self.gene1 = DenseGene(in_features=1, out_features=2, loc=self.loc1, dropout=True)
-        self.gene2 = DenseGene(in_features=1, out_features=4, loc=self.loc2, dropout=True)
+        self.exons = {"in_features": 10, "out_features": 110, "dropout": True}
+        self.gene = LinearGene(in_features=10, out_features=110, dropout=True)
 
     def test_init(self):
-        self.assertEqual(self.exons, self.gene1.exons)
-        self.assertEqual(self.loc1, self.gene1.location)
+        self.assertEqual(self.gene.exons, self.exons)
+
+        false_in_features = {"in_features": 1, "out_features": 100, "dropout": True}
+        self.assertRaises(ValueError, LinearGene, **false_in_features)
+
+        false_out_features = {"in_features": 9, "out_features": 100_000, "dropout": True}
+        self.assertRaises(ValueError, LinearGene, **false_out_features)
+
+        false_dropout = {"in_features": 9, "out_features": 100, "dropout": "hi"}
+        self.assertRaises(ValueError, LinearGene, **false_dropout)
 
     def test_mutate(self):
-        in_features = 1
-        out_features = 2
-        gene = DenseGene(in_features, out_features, loc=1)
-        gene.mutate(test_mutation)
-        self.assertEqual(gene.exons["in_features"], 1)
-        self.assertEqual(gene.exons["out_features"], 3)
-        self.assertEqual(gene.location, 1)
+        self.gene.mutate()
+        self.assertIn(self.gene.exons["out_features"], LinearGene.allowed_features)
+        self.assertTrue(self.gene.exons["in_features"] >= LinearGene.allowed_features[0])
 
     def test_len(self):
-        self.assertEqual(len(self.gene1), 2)
+        self.assertEqual(len(self.gene), 3)
+
+    def test_validate(self):
+        pass
+
+    def test_getattr(self):
+        self.assertEqual(self.gene.in_features, 10)
+        self.assertEqual(self.gene.out_features, 110)
+        self.assertEqual(self.gene.dropout, True)
+        self.assertEqual(self.gene.allowed_features, range(9, 10_000))
 
 
 if __name__ == "__main__":
