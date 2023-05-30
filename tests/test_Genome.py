3d1de325207f02fe3e6eb99c511acdf2ed9f1385
rkouidri <reuben-kouidri@hotmail.com>
2023-05-30 13:50:09 +0100

    added test_deepcopy

--- tests/test_Genome.py
+++ tests/test_Genome.py
@@ -1,9 +1,12 @@
 from dnasty.genetics import Genome, ConvBlock2dGene, MaxPool2dGene
 import coverage
 import unittest
+import copy
 
 
 class TestGenome(unittest.TestCase):
+    # TODO
+    #   - add test for __getattr__
 
     def setUp(self):
         self.conv = ConvBlock2dGene(1, 32, 10)
@@ -32,6 +35,17 @@ class TestGenome(unittest.TestCase):
 
     def test_outdims(self):
         self.assertEqual(self.genome.outdims, 55)
+
+    def test_deepcopy(self):
+        genome_copy = copy.deepcopy(self.genome)
+        self.assertNotEqual(genome_copy, self.genome)
+        self.assertNotEqual(genome_copy.genes, self.genome.genes)
+        for g1, g2 in zip(genome_copy.genes, self.genome.genes):
+            self.assertNotEqual(g1, g2)  # check the list of genes are not at the same memory address
+            for v1, v2 in zip(g1.exons.values(), g2.exons.values()):
+                self.assertEqual(v1, v2)  # check the values are the same
+            for k1, k2 in zip(g1.exons.keys(), g2.exons.keys()):
+                self.assertEqual(k1, k2)  # check the keys are the same
 
 
 if __name__ == "__main__":
