2e8c922592606630d15e8dba2dcac65ab7848445
Reuben Kouidri <reuben-kouidri@hotmail.com>
2024-01-11 08:23:22 +0000

    ConvBlockGene passing tests

--- tests/test_conv_block_gene.py
+++ tests/test_conv_block_gene.py
@@ -1,25 +1,52 @@
-from dnasty.genes.genetics import ConvBlock2dGene
+import torch
+from torch import nn
+
+from dnasty.genes.genes import ConvBlock2dGene
 import unittest
 
 
 class TestConvBlockGene(unittest.TestCase):
     def setUp(self):
-        self.exons = {"in_channels": 1, "out_channels": 2, "kernel_size": 3, "activation": "ReLU", "batch_norm": True}
-        self.gene = ConvBlock2dGene(in_channels=1, out_channels=2, kernel_size=3, activation="ReLU", batch_norm=True)
+        self.gene = ConvBlock2dGene(
+            in_channels=32,
+            out_channels=64,
+            kernel_size=5
+        )
 
     def test_init(self):
-        self.assertEqual(self.exons, self.gene.exons)
+        self.assertIsInstance(self.gene, ConvBlock2dGene)
+        self.assertEqual(self.gene.in_channels, 32)
+        self.assertEqual(self.gene.out_channels, 64)
+        self.assertEqual(self.gene.kernel_size, 5)
+        self.assertEqual(self.gene.activation, "ReLU")
+        self.assertTrue(self.gene.batch_norm)
 
     def test_getattr(self):
-        self.assertEqual(self.gene.in_channels, 1)
-        self.assertEqual(self.gene.out_channels, 2)
-        self.assertEqual(self.gene.kernel_size, 3)
-        self.assertEqual(self.gene.activation, "ReLU")
-        self.assertEqual(self.gene.batch_norm, True)
-        self.assertRaises(self.gene.unknown, AttributeError)
+        with self.assertRaises(AttributeError):
+            _ = self.gene.invalid
+
+    def test_express(self):
+        module = self.gene.express()
+        self.assertIsInstance(module, nn.Sequential)
+        self.assertIsInstance(module[0], nn.Conv2d)
+        self.assertEqual(module[0].in_channels, 32)
+        self.assertEqual(module[0].out_channels, 64)
+        self.assertEqual(module[0].kernel_size, (5, 5))
+        self.assertEqual(module[0].stride, (1, 1))
+        self.assertEqual(module[0].padding, (0, 0))
+        self.assertEqual(module[0].groups, 1)
+        self.assertEqual(module[0].bias.shape, torch.Size([64]))
+        self.assertIsInstance(module[1], nn.BatchNorm2d)
+        self.assertIsInstance(module[2], nn.ReLU)
+
+    def test_forward(self):
+        module = self.gene.express()
+        x = torch.randn(2, 32, 32, 32)
+        out = module(x)
+        self.assertEqual(out.shape, torch.Size([2, 64, 28, 28]))
 
     def test_len(self):
-        self.assertEqual(len(self.gene), len(self.exons))
+        self.assertEqual(len(self.gene), 5)
 
 
 if __name__ == "__main__":
