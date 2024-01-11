3e7f7c755f374af69e4935fcce973a36bf8fdfd6
Reuben Kouidri <reuben-kouidri@hotmail.com>
2024-01-11 07:37:55 +0000

    added types.py for common types

---
+++ dnasty/my_utils/types.py
@@ -0,0 +1,18 @@
+from typing import Optional, TypeVar, Union, Tuple
+
+
+T = TypeVar('T')
+_scalar_or_tuple_any_t = Union[T, Tuple[T, ...]]
+_scalar_or_tuple_1_t = Union[T, Tuple[T]]
+_scalar_or_tuple_2_t = Union[T, Tuple[T, T]]
+
+# For arguments which represent size parameters (eg, kernel size, padding)
+size_any_t = _scalar_or_tuple_any_t[int]
+size_1_t = _scalar_or_tuple_1_t[int]
+size_2_t = _scalar_or_tuple_2_t[int]
+
+
+# For arguments which represent optional size parameters
+# (eg, adaptive pool parameters, padding with default value, etc.)
+size_any_opt_t = _scalar_or_tuple_any_t[Optional[int]]
+size_2_opt_t = _scalar_or_tuple_2_t[Optional[int]]
