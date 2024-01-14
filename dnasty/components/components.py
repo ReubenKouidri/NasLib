0f344d2e4811044e97cc187a6cd82383c9d528d3
Reuben Kouidri <reuben-kouidri@hotmail.com>
2024-01-14 13:53:07 +0000

    optimised components

--- dnasty/components/components.py
+++ dnasty/components/components.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
-from typing import Union
-from dnasty.my_utils.types import size_2_t, size_2_opt_t
+from typing import Optional, Union
+from dnasty.my_utils import size_2_t, size_2_opt_t, act_t
 
 import torch
 from torch import Tensor
@@ -8,24 +8,63 @@ from torch import nn
 import torch.nn.functional as F
 
 
+def _get_activation(activation) -> nn.Module:
+    if activation is None:
+        return nn.ReLU()
+    elif isinstance(activation, str):
+        activation_module = getattr(nn, activation)()
+    elif isinstance(activation, nn.Module):
+        activation_module = activation
+    else:
+        raise TypeError("Activation must be a string, nn.Module, or None.")
+    return activation_module
+
+
 class LinearBlock(nn.Sequential):
+    """
+    Creates a sequential block: (Linear -> Activation -> Dropout)
+    Dropout optional with default probability of 0.5
+    (false for output layer).
+
+    Args:
+        in_features: The number of inputs to the linear layer.
+        out_features: The number of outputs from the linear layer.
+        dropout: If True, a dropout layer is added.
+        activation: The activation function (as a string or nn.Module).
+
+    Raises:
+        TypeError: If the activation type is not supported.
+    """
+
     def __init__(self,
                  in_features: int,
                  out_features: int,
                  dropout: bool = False,
-                 activation: str = None) -> None:
+                 activation: Optional[str, nn.Module] = None) -> None:
         super().__init__()
         self.add_module("linear", nn.Linear(in_features, out_features))
-
-        if activation is not None:
-            activation = getattr(nn, activation)()
-            self.add_module(f"{type(activation).__name__}", activation)
-
+        activation = _get_activation(activation)
+        self.add_module(type(activation).__name__, activation)
         if dropout:
             self.add_module("dropout", nn.Dropout(p=0.5))
 
 
 class ConvBlock2d(nn.Sequential):
+    """
+    Constructs a convolutional block with batch normalisation and activation.
+
+    Args:
+        in_channels: Number of channels in the input.
+        out_channels: Number of channels produced by the convolution.
+        kernel_size: Size of the convolving kernel.
+        stride: Stride of the convolution.
+        padding: Padding added to all four sides of the input.
+        activation: The activation function (as a string or nn.Module).
+
+    Raises:
+        ValueError: If padding type is not supported.
+    """
+
     def __init__(
             self,
             in_channels: int,
@@ -33,9 +72,7 @@ class ConvBlock2d(nn.Sequential):
             kernel_size: size_2_t,
             stride: size_2_opt_t = 1,
             padding: Union[str, size_2_t] = 0,
-            bn: bool = True,
-            activation: str = None
-    ) -> None:
+            activation: act_t = None) -> None:
         super().__init__()
         self.add_module("conv",
                         nn.Conv2d(in_channels=in_channels,
@@ -43,16 +80,10 @@ class ConvBlock2d(nn.Sequential):
                                   kernel_size=kernel_size,
                                   stride=stride,
                                   padding=padding))
-
-        if activation is not None:
-            activation = getattr(nn, activation)()
-            self.add_module(f"{type(activation).__name__}", activation)
-
-        if bn:
-            self.add_module("batch_norm",
-                            nn.BatchNorm2d(out_channels,
-                                           momentum=0.1,
-                                           affine=True))
+        activation = _get_activation(activation)
+        self.add_module(f"{type(activation).__name__}", activation)
+        self.add_module("batch_norm",
+                        nn.BatchNorm2d(out_channels, momentum=0.1, affine=True))
 
 
 class Flatten(nn.Module):
