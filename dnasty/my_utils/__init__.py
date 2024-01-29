3786d262814b99ad5f570031eecf75ce0cdcbacd
Reuben Kouidri <reuben-kouidri@hotmail.com>
2024-01-29 11:37:43 +0000

    my_utils/__init__.py updated

--- dnasty/my_utils/__init__.py
+++ dnasty/my_utils/__init__.py
@@ -1,6 +1,7 @@
 from .clock import clock
 from .config import Config
-from .ksplit import ksplit, load_2d_dataset
+from .ksplit import ksplit, load_2d_dataset, split_dataset
 from .stats import StatisticsReporter
 from .types import *
 from .wavelets import *
+from .my_utils import get_num_correct
