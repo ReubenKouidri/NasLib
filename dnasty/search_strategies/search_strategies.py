ae5299970a0fecdd35f4227a731c2fb883a51fda
Reuben Kouidri <reuben-kouidri@hotmail.com>
2024-01-29 11:48:38 +0000

    refactored and modulated RandomSearch class in search_strategies.py

--- dnasty/search_strategies/search_strategies.py
+++ dnasty/search_strategies/search_strategies.py
@@ -1,9 +1,13 @@
 import abc
 import copy
-import random
+import logging
 from dnasty.genetics import *
 from dnasty.estimators import EarlyStoppingEstimator
 from dnasty.my_utils import Config
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    datefmt='%d-%m-%Y %H:%M:%S')
 
 
 class SearchStrategyBase(abc.ABC):
@@ -27,72 +31,66 @@ class RandomSearch(SearchStrategyBase):
         Initialize a random search optimizer.
 
         Args:
-            config: total Config object
+            config (Config): configuration for NAS
         """
         super(RandomSearch, self).__init__()
+        self.search_space_cfg = config.search_space.cbam
         self.perf_metric = config.perf_metric
         self.population_size = config.population_size
-        self.population: list[Genome] = self._generate_population(
-            config.search_space.cbam)
+        self.generations = config.generations
+        self.num_classes = config.num_classes
         self.sampled: set[Genome] = set()
         self.history = []
         self.estimator = EarlyStoppingEstimator(config=config)
-        self.generations = config.generations
+        self.population: list[Genome] = self.generate_population(
+            config.search_space.cbam)
 
-    def fit(self):
-        for generation in range(self.generations):
+    def fit(self) -> None:
+        """Samples the search space for the given number of generations"""
+        for i, generation in enumerate(range(self.generations)):
+            logging.log(logging.INFO,
+                        f"Fitting generation: {i+1}/{self.generations}")
             self._step()
 
-    def _step(self):
-        """
-        Sample a new architecture to train.
-        """
-        population_best = []
-        for genome in self.population:
+    def _step(self) -> None:
+        """Sample and estimate performance of a new genome"""
+        for i, genome in enumerate(self.population):
+            logging.log(logging.INFO,
+                        f"Fitting genome: {i+1}/{len(self.population)}")
             genome.fitness = self.estimator.fit(genome)
-            self.sampled.add(copy.deepcopy(genome))
-            population_best.append(copy.deepcopy(genome))
 
+        self.sampled.update(
+            {copy.deepcopy(genome) for genome in self.population})
         self.history.append(
-            copy.deepcopy(max(population_best, key=lambda x: x.fitness)))
+            copy.deepcopy(max(self.population, key=lambda x: x.fitness)))
+        self.population = self.generate_population(self.search_space_cfg)
 
-    def _generate_population(self, cfg: Config) -> list[Genome]:
+    def generate_population(self, cfg: Config) -> list[Genome]:
         """
         Randomly initialise the population from the search space
-        cfg is the config for the search space e.g. cfg = nas.search_space.cbam
+
+        Args:
+            cfg (Config): the config for the search space
+                          e.g. cfg = nas.search_space.cbam
+
+        Returns:
+            list[Genome]: the initialised population of genomes
         """
         population = []
         while len(population) < self.population_size:
-            genes = []
-            num_cbam_genes = random.randint(1, cfg.cells)
-            for _ in range(num_cbam_genes):
-                cbam_gene = CBAMGene.from_random()
-                num_conv = random.randint(1, cfg.conv)
-                genes.extend(
-                    ConvBlock2dGene.from_random() for _ in range(num_conv))
-                genes.append(MaxPool2dGene.from_random())
-                genes.append(cbam_gene)
-
-            genes.append(FlattenGene())
-            genes.extend(
-                LinearBlockGene.from_random() for _ in range(cfg.linear))
-            genome = Genome.from_sequence(genes)
-            print(genome)
-
-            # TODO: lazy way to constrain num_params - needs investigation
-            if 2 < genome.outdims < 50:
-                population.append(Genome.from_sequence(genes))
-
+            genome = Genome.from_random(cfg)
+            if is_genome_valid(genome, cfg):
+                population.append(genome)
         return population
 
     @property
-    def generation_best(self):
+    def generation_best(self) -> Genome:
         """Returns best genome in the current generation"""
         return max(self.population, key=lambda x: x.fitness)
 
     @property
-    def fittest_genome(self):
+    def fittest_genome(self) -> Genome:
         """Returns the sampled architecture with the lowest validation error"""
         if len(self.history) == 0:
-            return None
+            raise ValueError("No history - call .fit() first")
         return max(self.history, key=lambda x: x.fitness)
