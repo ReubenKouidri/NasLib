5d2a42ee6cde031422a6a4c410cfa19ac50b0f7c
Reuben Kouidri <reuben-kouidri@hotmail.com>
2024-02-06 12:41:33 +0000

    cleaned part of ksplit

--- dnasty/my_utils/ksplit.py
+++ dnasty/my_utils/ksplit.py
@@ -1,24 +1,17 @@
 import random
 import warnings
 from torch.utils.data import Subset
-from typing import MutableSequence, TypeVar, Callable, Any
+from typing import MutableSequence, Callable, Any
 import collections.abc as abc
 import functools
 from datasets import CPSCDataset2D
 
+
 # TODO: This is an old file that needs to be revamped
-
-random.seed(9834275)
-
-T_co = TypeVar('T_co', covariant=True)
 
 
 def load_2d_dataset(data_path, ref_path) -> CPSCDataset2D:
     return CPSCDataset2D(data_path, ref_path)
-
-
-def load_datasets(data_path, ref_path):
-    return load_2d_dataset(data_path, ref_path)
 
 
 def split(dataset, ratio: tuple) -> tuple:
@@ -26,21 +19,18 @@ def split(dataset, ratio: tuple) -> tuple:
     indices = list(range(size))
     random.shuffle(indices)
 
-    split_sizes = [int(size * ratio[i]) for i in range(len(ratio))]  # either
-    if len(ratio) == 2:
-        train_indices = indices[:split_sizes[0]]
-        eval_indices = indices[split_sizes[0]:]
-        return train_indices, eval_indices
-    else:
-        train_indices = indices[0:split_sizes[0]]
-        eval_indices = indices[split_sizes[0]:split_sizes[0] + split_sizes[1]]
-        test_indices = indices[split_sizes[0] + split_sizes[1]:]
-
-        train_set = Subset(dataset, indices=list(train_indices))
-        eval_set = Subset(dataset, indices=list(eval_indices))
-        test_set = Subset(dataset, indices=list(test_indices))
-
+    if len(ratio) == 3:
+        train_ratio, eval_ratio, _ = ratio
+        train_size, eval_size = int(size * train_ratio), int(size * eval_ratio)
+        train_set = Subset(dataset, indices[:train_size])
+        eval_set = Subset(dataset, indices[train_size:train_size + eval_size])
+        test_set = Subset(dataset, indices[train_size + eval_size:])
         return train_set, eval_set, test_set
+    else:
+        train_size = int(size * ratio[0])
+        train_set = Subset(dataset, indices[:train_size])
+        eval_set = Subset(dataset, indices[train_size:])
+        return train_set, eval_set
 
 
 def split_dataset(dataset: CPSCDataset2D,
