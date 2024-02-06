d8abd53a48c0e3e9c587238944da66b683b5714d
Reuben Kouidri <reuben-kouidri@hotmail.com>
2024-02-06 12:40:09 +0000

    added tests for RandomSearch

--- tests/strategies/test_random_search.py
+++ tests/strategies/test_random_search.py
@@ -1,20 +1,65 @@
 import unittest
 from dnasty.my_utils.config import Config
 from dnasty.search_strategies import RandomSearch
+import dnasty
+import random
 
 
-class TestConfig(unittest.TestCase):
+class MockEarlyStoppingEstimator:
+    def __init__(self, config):
+        self.fidelity = 1
+        self.trainer = MockTrainer(config)
+
+    def fit(self, genome):
+        return self.trainer.fit(genome.to_module(), self.fidelity)  # fitness
+
+
+class MockTrainer:
+    def __init__(self, config: Config):
+        self.config = config
+
+    def fit(self, model, epochs):
+        return self._simulate_forward_pass()
+
+    @staticmethod
+    def _simulate_forward_pass():
+        """Simulate a forward pass with random metrics for testing."""
+        simulated_accuracy = random.uniform(0.5, 1.0)  # Example accuracy range
+        return simulated_accuracy
+
+
+class TestRandomSearch(unittest.TestCase):
+
     def setUp(self):
-        self.config = Config.from_file("../../dnasty/my_utils/config.json")
-        self.nas = self.config.nas
-        self.strat = RandomSearch(self.nas)
+        self.config = dnasty.my_utils.config.Config.from_file(
+            "../../dnasty/my_utils/config.json")
 
-    def test__generate_population(self):
-        self.assertEqual(len(self.strat.population), self.nas.population_size)
+        # Patch the dependent modules/methods used by RandomSearch
+        self.strategy = RandomSearch(self.config.nas)
+        self.strategy.estimator = MockEarlyStoppingEstimator(self.config)
 
-    def test_fit(self):
-        self.strat._step()
-        history = self.strat.history
+    def test_initialization(self):
+        """Test RandomSearch initialization with config."""
+        self.assertEqual(self.strategy.population_size,
+                         self.config.nas.population_size)
+
+    def test_fit_generates_history(self):
+        """Test that the fit method generates a history of genomes."""
+        self.strategy.fit()
+        self.assertTrue(len(self.strategy.history) > 0)
+
+    def test_ranking_of_genomes(self):
+        """Test that genomes are ranked based on fitness."""
+        self.strategy.fit()
+        self.assertEqual(self.strategy.generation_best,
+                         self.strategy.population[0])
+        genomes = sorted(self.strategy.history, key=lambda x: x.fitness,
+                         reverse=True)
+
+        self.assertEqual(len(genomes), self.config.nas.population_size)
+        self.assertEqual(genomes[0], self.strategy.fittest_genome)
+        self.assertEqual(genomes[0].fitness,
+                         self.strategy.fittest_genome.fitness)
 
 
 if __name__ == '__main__':
